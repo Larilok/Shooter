635ec106255659e76c4186e90a69d9ab1c08ab1a
Larilok <michaelmarkelovv@gmail.com>
2020-11-25 17:45:20 +0200

    add client constructor

--- server/Client.cs
+++ server/Client.cs
@@ -2,7 +2,16 @@ namespace server
 {
   public class Client
   {
-    
+
+    public int id; 
+    public UDP udp;
+
+    public Client(int clientId)
+    {
+      id = clientId;
+      udp = new UDP(id);
+        
+    } 
     public class UDP
     {
       public UdpClient socket;
